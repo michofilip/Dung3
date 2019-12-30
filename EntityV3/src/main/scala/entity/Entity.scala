7f0e04a6dcdde6837f60d330379474e68c3d5b3b
majki <michofilip@gmail.com>
2019-12-30 20:58:22 +0100

    repo

--- EntityV3/src/main/scala/entity/Entity.scala
+++ EntityV3/src/main/scala/entity/Entity.scala
@@ -1,18 +1,9 @@
 package entity
 
 import commons.temporal.Timestamp
-import model.{Position, StateHolder}
+import model.{Coordinates, Direction, State}
 
 case class Entity(id: Long, timestamp: Timestamp,
-                  stateHolderOpt: Option[StateHolder] = None,
-                  positionOpt: Option[Position] = None) {
-    
-    def setStateHolder(stateHolder: StateHolder): Entity = copy(stateHolderOpt = Some(stateHolder))
-    
-    def setPosition(position: Position): Entity = copy(positionOpt = Some(position))
-    
-    def removeStateHolder(): Entity = copy(stateHolderOpt = None)
-    
-    def removePosition(): Entity = copy(positionOpt = None)
-    
-}
+                  stateOpt: Option[State] = None,
+                  coordinatesOpt: Option[Coordinates] = None,
+                  directionOpt: Option[Direction] = None)
--- EntityV3/src/main/scala/model/StateHolder.scala
+++
@@ -1,5 +0,0 @@
-package model
-
-import commons.temporal.Timestamp
-
-case class StateHolder(state:State, stateTimestamp:Timestamp)
