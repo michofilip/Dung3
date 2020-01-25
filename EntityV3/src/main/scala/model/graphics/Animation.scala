4bdcb67d20648edb633892005b5babe7b81224ad
majki <michofilip@gmail.com>
2020-01-25 21:34:25 +0100

    animation fix

--- EntityV3/src/main/scala/model/graphics/Animation.scala
+++ EntityV3/src/main/scala/model/graphics/Animation.scala
@@ -4,17 +4,21 @@ import commons.temporal.Duration
 import commons.utils.MathUtils.{bound, floor, mod}
 
 sealed abstract class Animation {
+    val length: Int
+    
     def frame(duration: Duration): Frame
 }
 
 object Animation {
     
     final case class SingleFrameAnimation(frame: Frame) extends Animation {
+        override val length: Int = 1
+        
         override def frame(duration: Duration): Frame = frame
     }
     
     final case class OneWayAnimation(fps: Double, frames: Vector[Frame]) extends Animation {
-        private val length = frames.length
+        override val length: Int = frames.length
         
         override def frame(duration: Duration): Frame = {
             val frameNo = floor(duration.time * fps / 1000)
@@ -24,7 +28,7 @@ object Animation {
     }
     
     final case class LoopingAnimation(fps: Double, frames: Vector[Frame]) extends Animation {
-        private val length = frames.length
+        override val length: Int = frames.length
         
         override def frame(duration: Duration): Frame = {
             val frameNo = floor(duration.time * fps / 1000)
