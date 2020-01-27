7e1f69e6bcd922791174c278cd864e2698a03f25
majki <michofilip@gmail.com>
2020-01-27 06:31:15 +0100

    entity rebuild

--- EntityV3/src/main/scala/entity/Entity.scala
+++ EntityV3/src/main/scala/entity/Entity.scala
@@ -11,48 +11,56 @@ case class Entity(id: Long, name: String, initialTimestamp: Timestamp,
                   positionOpt: Option[Position] = None,
                   physicsContainerOpt: Option[PhysicsContainer] = None,
                   graphicsOpt: Option[Graphics] = None
-                 ) {
-    def setState(stateContainer: StateContainer): Entity = copy(stateContainerOpt = Some(stateContainer))
+                 )
+
+object Entity {
     
-    def setPosition(position: Position): Entity = copy(positionOpt = Some(position))
+    implicit class StateService(entity: Entity) {
+        def setState(stateContainer: StateContainer): Entity = entity.copy(stateContainerOpt = Some(stateContainer))
+        
+        def removeState(): Entity = entity.copy(stateContainerOpt = None)
+        
+        def updateState(stateMapper: State => State, timestamp: Timestamp): Entity =
+            entity.stateContainerOpt
+                    .map(StateContainer.update(stateMapper, timestamp))
+                    .map(setState)
+                    .getOrElse(entity)
+    }
     
-    def setPhysicsContainer(physicsContainer: PhysicsContainer): Entity = copy(physicsContainerOpt = Some(physicsContainer))
+    implicit class PositionService(entity: Entity) {
+        def setPosition(position: Position): Entity = entity.copy(positionOpt = Some(position))
+        
+        def removePosition(): Entity = entity.copy(positionOpt = None)
+        
+        def updatePosition(positionMapper: (Coordinates, Direction) => (Coordinates, Direction), timestamp: Timestamp): Entity =
+            entity.positionOpt
+                    .map(Position.update(positionMapper, timestamp))
+                    .map(setPosition)
+                    .getOrElse(entity)
+    }
     
-    def setGraphics(graphics: Graphics): Entity = copy(graphicsOpt = Some(graphics))
+    implicit class PhysicsService(entity: Entity) {
+        def setPhysicsContainer(physicsContainer: PhysicsContainer): Entity = entity.copy(physicsContainerOpt = Some(physicsContainer))
+        
+        def removePhysicsContainer(): Entity = entity.copy(physicsContainerOpt = None)
+        
+        def selectPhysics(): Entity =
+            entity.physicsContainerOpt
+                    .map(PhysicsContainer.selectPhysics(entity))
+                    .map(setPhysicsContainer)
+                    .getOrElse(entity)
+    }
     
-    
-    def removeState(): Entity = copy(stateContainerOpt = None)
-    
-    def removePosition(): Entity = copy(positionOpt = None)
-    
-    def removePhysicsContainer(): Entity = copy(physicsContainerOpt = None)
-    
-    def removeGraphics(): Entity = copy(graphicsOpt = None)
-    
-    
-    def updateState(stateMapper: State => State, timestamp: Timestamp): Entity =
-        stateContainerOpt
-                .map(StateContainer.update(stateMapper, timestamp))
-                .map(setState)
-                .getOrElse(this)
-    
-    def updatePosition(positionMapper: (Coordinates, Direction) => (Coordinates, Direction), timestamp: Timestamp): Entity =
-        positionOpt
-                .map(Position.update(positionMapper, timestamp))
-                .map(setPosition)
-                .getOrElse(this)
-    
-    
-    def selectPhysics(): Entity =
-        physicsContainerOpt
-                .map(PhysicsContainer.selectPhysics(this))
-                .map(setPhysicsContainer)
-                .getOrElse(this)
-    
-    def selectAnimation(): Entity =
-        graphicsOpt
-                .map(Graphics.selectGraphics(this))
-                .map(setGraphics)
-                .getOrElse(this)
+    implicit class GraphicsService(entity: Entity) {
+        def setGraphics(graphics: Graphics): Entity = entity.copy(graphicsOpt = Some(graphics))
+        
+        def removeGraphics(): Entity = entity.copy(graphicsOpt = None)
+        
+        def selectAnimation(): Entity =
+            entity.graphicsOpt
+                    .map(Graphics.selectGraphics(entity))
+                    .map(setGraphics)
+                    .getOrElse(entity)
+    }
     
 }
