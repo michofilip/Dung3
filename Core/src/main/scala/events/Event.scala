d3a204a07e5fd99e40ab66933c967d6f79278eef
Michał Filipowski <michofilip@gmail.com>
2020-02-03 21:59:32 +0100

    Events update (#3)
    * import fix
    
    * EventResponse added

--- Core/src/main/scala/events/Event.scala
+++ Core/src/main/scala/events/Event.scala
@@ -1,6 +1,7 @@
 package events
 
 import entity.Entity
+import events.Event.EventResponse
 import world.WorldFrameContext
 
 import scala.language.implicitConversions
@@ -8,11 +9,23 @@ import scala.language.implicitConversions
 abstract class Event {
     val entityId: Long
     
-    def applyTo(entity: Entity)(implicit wfc: WorldFrameContext): (Vector[Entity], Vector[Event])
+    def applyTo(entity: Entity)(implicit wfc: WorldFrameContext): EventResponse
 }
 
 object Event {
+    type EventResponse = (Vector[Entity], Vector[Event])
+    
     implicit def en2Vector(entity: Entity): Vector[Entity] = Vector[Entity](entity)
     
     implicit def ev2Vector(event: Event): Vector[Event] = Vector(event)
+    
+    implicit def en2EventResponse(entity: Entity): EventResponse = (entity, Vector.empty)
+    
+    implicit def ens2EventResponse(entities: Vector[Entity]): EventResponse = (entities, Vector.empty)
+    
+    implicit def ev2EventResponse(event: Event): EventResponse = (Vector.empty, event)
+    
+    implicit def evs2EventResponse(events: Vector[Event]): EventResponse = (Vector.empty, events)
+    
+    
 }
--- Core/src/main/scala/events/PositionEvents.scala
+++ Core/src/main/scala/events/PositionEvents.scala
@@ -10,27 +10,22 @@ import world.WorldFrameContext
 object PositionEvents {
     
     final case class MoveTo(override val entityId: Long, x: Int, y: Int) extends Event {
-        override def applyTo(entity: Entity)(implicit wfc: WorldFrameContext): (Vector[Entity], Vector[Event]) = {
-            val newEntity = entity.updatePosition(PositionMappers.moveTo(x, y), wfc.timestamp)
-            (newEntity, Vector.empty)
+        override def applyTo(entity: Entity)(implicit wfc: WorldFrameContext): EventResponse = {
+            entity.updatePosition(PositionMappers.moveTo(x, y), wfc.timestamp)
         }
     }
     
     final case class MoveBy(override val entityId: Long, dx: Int, dy: Int) extends Event {
-        override def applyTo(entity: Entity)(implicit wfc: WorldFrameContext): (Vector[Entity], Vector[Event]) = {
-            val newEntity = entity.updatePosition(PositionMappers.moveBy(dx, dy), wfc.timestamp)
-            (newEntity, Vector.empty)
+        override def applyTo(entity: Entity)(implicit wfc: WorldFrameContext): EventResponse = {
+            entity.updatePosition(PositionMappers.moveBy(dx, dy), wfc.timestamp)
         }
     }
     
     final case class Step(override val entityId: Long, direction: Direction) extends Event {
-        override def applyTo(entity: Entity)(implicit wfc: WorldFrameContext): (Vector[Entity], Vector[Event]) = {
-            val positionMapper = PositionMappers.step(direction) --> PositionMappers.rotateTo(direction)
-            
-            val newEntity = entity
-                    .updatePosition(positionMapper, wfc.timestamp)
+        override def applyTo(entity: Entity)(implicit wfc: WorldFrameContext): EventResponse = {
+            import PositionMappers._
+            entity.updatePosition(step(direction) --> rotateTo(direction), wfc.timestamp)
                     .updateState(StateMappers.movement, wfc.timestamp)
-            (newEntity, Vector.empty)
         }
     }
     
