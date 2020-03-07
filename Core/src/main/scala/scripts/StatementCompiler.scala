d03957e8d199347b991438c9ed638549cd9f4316
majki <michofilip@gmail.com>
2020-03-07 22:18:57 +0100

    label to labelId rename

--- Core/src/main/scala/scripts/StatementCompiler.scala
+++ Core/src/main/scala/scripts/StatementCompiler.scala
@@ -16,121 +16,121 @@ object StatementCompiler {
         instructions
     }
     
-    private def compile(statement: Statement, instructions: Vector[Instruction], label: Int): (Vector[Instruction], Int) = {
+    private def compile(statement: Statement, instructions: Vector[Instruction], labelId: Int): (Vector[Instruction], Int) = {
         statement match {
             case Execute(events) =>
-                compileExecute(events, instructions, label)
+                compileExecute(events, instructions, labelId)
             case Block(statements) =>
-                compileBlock(statements, instructions, label)
+                compileBlock(statements, instructions, labelId)
             case MultiWhenTherefore(whenThereforeSeq) =>
-                compileWhen(whenThereforeSeq, Block(Vector.empty), instructions, label)
+                compileWhen(whenThereforeSeq, Block(Vector.empty), instructions, labelId)
             case MultiWhenThereforeOtherwise(whenThereforeSeq, otherwiseStatement) =>
-                compileWhen(whenThereforeSeq, otherwiseStatement, instructions, label)
+                compileWhen(whenThereforeSeq, otherwiseStatement, instructions, labelId)
             case LoopBody(condition, body) =>
-                compileLoop(condition, body, instructions, label)
+                compileLoop(condition, body, instructions, labelId)
             case ChooseVariants(value, variants) =>
-                compileChoose(value, variants, Block(Vector.empty), instructions, label)
+                compileChoose(value, variants, Block(Vector.empty), instructions, labelId)
             case ChooseVariantsOtherwise(value, variants, otherwise) =>
-                compileChoose(value, variants, otherwise, instructions, label)
+                compileChoose(value, variants, otherwise, instructions, labelId)
         }
     }
     
     private def compileExecute(events: Events,
-                               instructions: Vector[Instruction], label: Int): (Vector[Instruction], Int) = {
-        (instructions :+ EXECUTE(events), label)
+                               instructions: Vector[Instruction], labelId: Int): (Vector[Instruction], Int) = {
+        (instructions :+ EXECUTE(events), labelId)
     }
     
     private def compileBlock(statements: Vector[Statement],
-                             instructions: Vector[Instruction], label: Int): (Vector[Instruction], Int) = {
-        val (newInstructions, afterBlockLabel) = statements.foldLeft((instructions, label)) {
-            case ((instructions, label), statement) => compile(statement, instructions, label)
+                             instructions: Vector[Instruction], labelId: Int): (Vector[Instruction], Int) = {
+        val (newInstructions, afterBlockLabelId) = statements.foldLeft((instructions, labelId)) {
+            case ((instructions, labelId), statement) => compile(statement, instructions, labelId)
         }
         
-        (instructions ++ newInstructions, afterBlockLabel)
+        (instructions ++ newInstructions, afterBlockLabelId)
     }
     
     private def compileWhenThereforeSeq(whenThereforeSeq: Vector[WhenTherefore], otherwiseStatement: Statement,
-                                        label: Int): (Vector[Instruction], Int) = {
+                                        labelId: Int): (Vector[Instruction], Int) = {
         whenThereforeSeq match {
             case WhenTherefore(condition, thereforeStatement) +: rest =>
-                val elseLabel = label
-                val exitLabel = label + 1
+                val elseLabelId = labelId
+                val exitLabelId = labelId + 1
                 
-                val (thenInstructions, afterThenLabel) = compile(thereforeStatement, Vector.empty, exitLabel + 1)
-                val (elseInstructions, afterElseLabel) = compileWhenThereforeSeq(rest, otherwiseStatement, afterThenLabel + 1)
+                val (thenInstructions, afterThenLabelId) = compile(thereforeStatement, Vector.empty, exitLabelId + 1)
+                val (elseInstructions, afterElseLabelId) = compileWhenThereforeSeq(rest, otherwiseStatement, afterThenLabelId + 1)
                 
                 val newInstructions = TEST(condition) ++
-                        GOTO(elseLabel) ++
+                        GOTO(elseLabelId) ++
                         thenInstructions ++
-                        GOTO(exitLabel) ++
-                        LABEL(elseLabel) ++
+                        GOTO(exitLabelId) ++
+                        LABEL(elseLabelId) ++
                         elseInstructions ++
-                        LABEL(exitLabel)
+                        LABEL(exitLabelId)
                 
-                (newInstructions, afterElseLabel)
+                (newInstructions, afterElseLabelId)
             case _ =>
-                compile(otherwiseStatement, Vector.empty, label)
+                compile(otherwiseStatement, Vector.empty, labelId)
         }
     }
     
     private def compileWhen(whenThereforeSeq: Vector[WhenTherefore], otherwiseStatement: Statement,
-                            instructions: Vector[Instruction], label: Int): (Vector[Instruction], Int) = {
-        val (newInstructions, afterWhenLabel) =
-            compileWhenThereforeSeq(whenThereforeSeq, otherwiseStatement, label)
+                            instructions: Vector[Instruction], labelId: Int): (Vector[Instruction], Int) = {
+        val (newInstructions, afterWhenLabelId) =
+            compileWhenThereforeSeq(whenThereforeSeq, otherwiseStatement, labelId)
         
-        (instructions ++ newInstructions, afterWhenLabel)
+        (instructions ++ newInstructions, afterWhenLabelId)
     }
     
     private def compileLoop(condition: BooleanValue, body: Statement,
-                            instructions: Vector[Instruction], label: Int): (Vector[Instruction], Int) = {
-        val loopLabel = label
-        val exitLabel = label + 1
+                            instructions: Vector[Instruction], labelId: Int): (Vector[Instruction], Int) = {
+        val loopLabelId = labelId
+        val exitLabelId = labelId + 1
         
-        val (loopedInstructions, afterLoopLabelId) = compile(body, Vector.empty, exitLabel + 1)
+        val (loopedInstructions, afterLoopLabelIdId) = compile(body, Vector.empty, exitLabelId + 1)
         
-        val newInstructions = LABEL(loopLabel) ++
+        val newInstructions = LABEL(loopLabelId) ++
                 TEST(condition) ++
-                GOTO(exitLabel) ++
+                GOTO(exitLabelId) ++
                 loopedInstructions ++
-                GOTO(loopLabel) ++
-                LABEL(exitLabel)
+                GOTO(loopLabelId) ++
+                LABEL(exitLabelId)
         
-        (instructions ++ newInstructions, afterLoopLabelId)
+        (instructions ++ newInstructions, afterLoopLabelIdId)
     }
     
-    private def compileVariant(variantWhenTherefore: VariantWhenTherefore, chooseValue: Value, exitLabel: Int,
-                               label: Int): (Vector[Instruction], Int) = {
-        val variantExitLabel = label
-        val (variantInstructions, afterVariantLabel) = compile(variantWhenTherefore.therefore, Vector.empty, variantExitLabel + 1)
+    private def compileVariant(variantWhenTherefore: VariantWhenTherefore, chooseValue: Value, exitLabelId: Int,
+                               labelId: Int): (Vector[Instruction], Int) = {
+        val variantExitLabelId = labelId
+        val (variantInstructions, afterVariantLabelId) = compile(variantWhenTherefore.therefore, Vector.empty, variantExitLabelId + 1)
         
         val condition = variantWhenTherefore.values
                 .map(value => chooseValue === value)
                 .reduceLeft(_ || _) && variantWhenTherefore.condition
         
         val newInstructions = TEST(condition) ++
-                GOTO(variantExitLabel) ++
+                GOTO(variantExitLabelId) ++
                 variantInstructions ++
-                GOTO(exitLabel) ++
-                LABEL(variantExitLabel)
+                GOTO(exitLabelId) ++
+                LABEL(variantExitLabelId)
         
-        (newInstructions, afterVariantLabel)
+        (newInstructions, afterVariantLabelId)
     }
     
     private def compileChoose(value: Value, variants: Vector[VariantWhenTherefore], otherwise: Statement,
-                              instructions: Vector[Instruction], label: Int): (Vector[Instruction], Int) = {
-        val exitLabel = label
+                              instructions: Vector[Instruction], labelId: Int): (Vector[Instruction], Int) = {
+        val exitLabelId = labelId
         
-        val (variantInstructions, afterVariantLabel) = variants.foldLeft((Vector.empty[Instruction], exitLabel + 1)) {
-            case ((instructions, label), variant) =>
-                val (newInstructions, afterVariantLabel) = compileVariant(variant, value, exitLabel, label)
-                (instructions ++ newInstructions, afterVariantLabel)
+        val (variantInstructions, afterVariantLabelId) = variants.foldLeft((Vector.empty[Instruction], exitLabelId + 1)) {
+            case ((instructions, labelId), variant) =>
+                val (newInstructions, afterVariantLabelId) = compileVariant(variant, value, exitLabelId, labelId)
+                (instructions ++ newInstructions, afterVariantLabelId)
         }
-        val (otherwiseInstructions, afterOtherwiseLabel) = compile(otherwise, Vector.empty, afterVariantLabel)
+        val (otherwiseInstructions, afterOtherwiseLabelId) = compile(otherwise, Vector.empty, afterVariantLabelId)
         
         val newInstructions = variantInstructions ++
                 otherwiseInstructions ++
-                LABEL(exitLabel)
+                LABEL(exitLabelId)
         
-        (instructions ++ newInstructions, afterOtherwiseLabel)
+        (instructions ++ newInstructions, afterOtherwiseLabelId)
     }
 }
