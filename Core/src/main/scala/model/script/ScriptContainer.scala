e10e66a68208daddc4b3ca56b98bcf1bdb2aa79c
Michał Filipowski <michofilip@gmail.com>
2020-06-13 22:14:04 +0200

    old modules removed (#13)

--- Core/src/main/scala/model/script/ScriptContainer.scala
+++
@@ -1,6 +0,0 @@
-package model.script
-
-case class ScriptContainer(scripts: Map[String, Script]) {
-    def getScript(scriptName: String): Option[Script] =
-        scripts.get(scriptName)
-}
--- Core/src/main/scala/world/WorldFrameContext.scala
+++
@@ -1,7 +0,0 @@
-package world
-
-import commons.temporal.Timestamp
-import repository.EntityRepository
-import value.ValueContext
-
-case class WorldFrameContext(timestamp: Timestamp, turn: Int, entityRepository: EntityRepository) extends ValueContext
