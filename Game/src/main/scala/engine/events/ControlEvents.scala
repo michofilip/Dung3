f640a670376b149b52834931a23f2c4e2fe4ae2a
majki <michofilip@gmail.com>
2020-06-14 14:26:32 +0200

    ControlEvents fixed

--- Game/src/main/scala/engine/events/ControlEvents.scala
+++ Game/src/main/scala/engine/events/ControlEvents.scala
@@ -9,64 +9,76 @@ object ControlEvents {
 
     final case class Chain(override val entityId: Long, eventsSeq: Seq[Events]) extends Event {
         override def applyTo(entity: Entity)(implicit gc: GameContext): EventResponse = {
-            EventResponse().withEvents {
-                eventsSeq match {
-                    case Nil => Vector.empty
-                    case events +: Nil => events
-                    case events +: rest => events :+ Chain(entityId, rest)
+            EventResponse()
+                .withEntity(entity)
+                .withEvents {
+                    eventsSeq match {
+                        case Nil => Vector.empty
+                        case events +: Nil => events
+                        case events +: rest => events :+ Chain(entityId, rest)
+                    }
                 }
-            }
         }
     }
 
     final case class Repeat(override val entityId: Long, repetitions: Int, events: Events) extends Event {
         override def applyTo(entity: Entity)(implicit gc: GameContext): EventResponse = {
-            EventResponse().withEvents {
-                if (repetitions >= 2)
-                    events :+ Repeat(entityId, repetitions - 1, events)
-                else if (repetitions == 1)
-                    events
-                else
-                    Vector.empty
-            }
+            EventResponse()
+                .withEntity(entity)
+                .withEvents {
+                    if (repetitions >= 2)
+                        events :+ Repeat(entityId, repetitions - 1, events)
+                    else if (repetitions == 1)
+                        events
+                    else
+                        Vector.empty
+                }
         }
     }
 
     final case class ScheduleTime(override val entityId: Long, timestamp: Timestamp, events: Events) extends Event {
         override def applyTo(entity: Entity)(implicit gc: GameContext): EventResponse = {
-            EventResponse().withEvents {
-                if (gc.timestamp >= timestamp)
-                    events
-                else
-                    Vector(this)
-            }
+            EventResponse()
+                .withEntity(entity)
+                .withEvents {
+                    if (gc.timestamp >= timestamp)
+                        events
+                    else
+                        Vector(this)
+                }
         }
     }
 
     final case class ScheduleTurn(override val entityId: Long, turn: Int, events: Events) extends Event {
         override def applyTo(entity: Entity)(implicit gc: GameContext): EventResponse = {
-            EventResponse().withEvents {
-                if (gc.turn >= turn)
-                    events
-                else
-                    Vector(this)
-            }
+            EventResponse()
+                .withEntity(entity)
+                .withEvents {
+                    if (gc.turn >= turn)
+                        events
+                    else
+                        Vector(this)
+                }
         }
     }
 
     final case class DelayTime(override val entityId: Long, duration: Duration, events: Events) extends Event {
         override def applyTo(entity: Entity)(implicit gc: GameContext): EventResponse = {
-            EventResponse().withEvent {
-                ScheduleTime(entityId, gc.timestamp + duration, events)
-            }
+            EventResponse()
+                .withEntity(entity)
+                .withEvent {
+                    ScheduleTime(entityId, gc.timestamp + duration, events)
+                }
         }
     }
 
     final case class DelayTurn(override val entityId: Long, turns: Int, events: Events) extends Event {
         override def applyTo(entity: Entity)(implicit gc: GameContext): EventResponse = {
-            EventResponse().withEvent {
-                ScheduleTurn(entityId, gc.turn + turns, events)
-            }
+            EventResponse()
+                .withEntity(entity)
+                .withEvent {
+                    ScheduleTurn(entityId, gc.turn + turns, events)
+                }
         }
     }
 
